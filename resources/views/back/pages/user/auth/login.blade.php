f167fa89260997337e2e2334c6076af5ddea6249
jjd214 <johnjacobdimaya0@gmail.com>
2025-01-11 20:28:59 +0800

    implements design

--- resources/views/back/pages/user/auth/login.blade.php
+++ resources/views/back/pages/user/auth/login.blade.php
@@ -1,63 +1,64 @@
 @extends('back.layout.auth-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Login')
 @section('content')
-<div class="login-box bg-white box-shadow border-radius-10">
-    <div class="login-title">
-        <h2 class="text-center text-primary">User Login</h2>
+    <div class="login-box bg-white box-shadow border-radius-10">
+        <div class="login-title">
+            <h2 class="text-center text-dark">User Login</h2>
+        </div>
+        <form action="{{ route('consignor.login-handler') }}" method="POST">
+            <x-form-alerts></x-form-alerts>
+            @csrf
+            <div class="input-group custom mb-1">
+                <input type="text" class="form-control form-control-lg" placeholder="Username / Email" name="login_id"
+                    value="{{ old('login_id') }}">
+                <div class="input-group-append custom">
+                    <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+                </div>
+            </div>
+            @error('login_id')
+                <span class="text-danger ml-1">{{ $message }}</span>
+            @enderror
+            <div class="input-group custom mb-1 mt-2">
+                <input type="password" class="form-control form-control-lg" placeholder="**********" name="password">
+                <div class="input-group-append custom">
+                    <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+                </div>
+            </div>
+            @error('password')
+                <span class="text-danger ml-1">{{ $message }}</span>
+            @enderror
+            <div class="row pb-30">
+                <div class="col-6">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                        <label class="custom-control-label" for="customCheck1">Remember</label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="forgot-password">
+                        <a href="{{ route('consignor.forgot-password') }}">Forgot Password</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="input-group mb-0">
+                        <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
+                    </div>
+                </div>
+            </div>
+
+            <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373" style="color: rgb(112, 115, 115);">
+                OR
+            </div>
+
+            <div class="input-group mb-0" style="margin-top: -15px;">
+                <a href="{{ route('google-auth') }}" class="btn btn-dark btn-block my-3">
+                    <i class="bi bi-google"></i> Sign in with Google
+                </a>
+            </div>
+
+        </form>
     </div>
-    <form action="{{ route('consignor.login-handler') }}" method="POST">
-        <x-form-alerts></x-form-alerts>
-        @csrf
-        <div class="input-group custom mb-1">
-            <input type="text" class="form-control form-control-lg" placeholder="Username / Email" name="login_id" value="{{ old('login_id') }}">
-            <div class="input-group-append custom">
-                <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
-            </div>
-        </div>
-        @error('login_id')
-            <span class="text-danger ml-1">{{ $message }}</span>
-        @enderror
-        <div class="input-group custom mb-1 mt-2">
-            <input type="password" class="form-control form-control-lg" placeholder="**********" name="password">
-            <div class="input-group-append custom">
-                <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
-            </div>
-        </div>
-        @error('password')
-            <span class="text-danger ml-1">{{ $message }}</span>
-        @enderror
-        <div class="row pb-30">
-            <div class="col-6">
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                    <label class="custom-control-label" for="customCheck1">Remember</label>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="forgot-password">
-                    <a href="{{ route('consignor.forgot-password') }}">Forgot Password</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="input-group mb-0">
-                    <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
-                </div>
-            </div>
-        </div>
-
-        <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373" style="color: rgb(112, 115, 115);">
-            OR
-        </div>
-
-        <div class="input-group mb-0" style="margin-top: -15px;">
-            <a href="{{ route('google-auth') }}" class="btn btn-danger btn-block my-3">
-                <i class="bi bi-google"></i> Sign in with Google
-            </a>
-        </div>
-
-    </form>
-</div>
 @endsection
--- resources/views/back/pages/user/auth/register.blade.php
+++ resources/views/back/pages/user/auth/register.blade.php
@@ -2,54 +2,58 @@
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Register')
 @section('content')
 
-<div class="login-box bg-white box-shadow border-radius-10">
-    <div class="login-title">
-        <h2 class="text-center text-primary">Create Consignor Account</h2>
-    </div>
-    <form action="{{ route('consignor.create') }}" method="POST">
-        @csrf
-        <x-form-alerts></x-form-alerts>
+    <div class="login-box bg-white box-shadow border-radius-10">
+        <div class="login-title">
+            <h2 class="text-center text-dark">Create Consignor Account</h2>
+        </div>
+        <form action="{{ route('consignor.create') }}" method="POST">
+            @csrf
+            <x-form-alerts></x-form-alerts>
 
-        <div class="form-group">
-            <label for="">Full name: </label>
-            <input type="text" class="form-control" placeholder="Enter full name" name="name" value="{{ old('name') }}">
-            @error('name')
-                <span class="text-danger ml-2">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="">Email: </label>
-            <input type="text" class="form-control" placeholder="Enter email address" name="email" value="{{ old('email') }}">
-            @error('email')
-                <span class="text-danger ml-2">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="">Password: </label>
-            <input type="password" class="form-control" placeholder="Enter password" name="password">
-            @error('password')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="">Confirm password: </label>
-            <input type="password" class="form-control" placeholder="Retype password" name="confirm_password">
-            @error('confirm_password')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="form-group">
+                <label for="">Full name: </label>
+                <input type="text" class="form-control" placeholder="Enter full name" name="name"
+                    value="{{ old('name') }}">
+                @error('name')
+                    <span class="text-danger ml-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="">Email: </label>
+                <input type="text" class="form-control" placeholder="Enter email address" name="email"
+                    value="{{ old('email') }}">
+                @error('email')
+                    <span class="text-danger ml-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="">Password: </label>
+                <input type="password" class="form-control" placeholder="Enter password" name="password">
+                @error('password')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="">Confirm password: </label>
+                <input type="password" class="form-control" placeholder="Retype password" name="confirm_password">
+                @error('confirm_password')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="input-group mb-0">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Create account</button>
-                </div>
-                <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373" style="color: rgb(112, 115, 115)">OR</div>
-                <div class="input-group mb-0">
-                    <a href="{{ route('consignor.login') }}" class="btn btn-outline-primary btn-lg btn-block">Sign In</a>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="input-group mb-0">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Create account</button>
+                    </div>
+                    <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373"
+                        style="color: rgb(112, 115, 115)">OR</div>
+                    <div class="input-group mb-0">
+                        <a href="{{ route('consignor.login') }}" class="btn btn-outline-primary btn-lg btn-block">Sign
+                            In</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
